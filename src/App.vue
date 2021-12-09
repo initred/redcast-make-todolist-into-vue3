462689b57302ebd4e2bceca27d497c12dc157ab2
InitRed <initred@gmail.com>
2021-12-09 21:39:50 +0900

    1 - write TodoList.vue Component

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+<script setup>
+const items = [];
+
+function add() {
+  console.log("add");
+
+  const todoText = document.getElementById("todo-input").value;
+
+  items.push(todoText);
+}
+
+function remove(itemsIndex) {
+  console.log("remove");
+
+  items.splice(itemsIndex, 1);
+}
+</script>
+
+<template>
+  <div class="todo-list">
+    <h1 class="todo-list-h1">Todo List</h1>
+    <div class="todo-list-add">
+      <input type="text" id="todo-input" placeholder="add items" />
+      <button type="button" @click="add">추가</button>
+    </div>
+    <div class="todo-items">
+      <div v-for="(item, index) in items" :key="index">
+        <div>{{ index + 1 }} - {{ item }}</div>
+        <button type="button" @click="remove">삭제</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+html,
+body,
+#app {
+  width: 100%;
+  height: 100%;
+  margin: 0px;
+  padding: 0px;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.todo-list {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.todo-list .todo-list-h1 {
+  font-size: 24px;
+}
+
+.todo-list .todo-list-add {
+  display: flex;
+}
+
+.todo-list .todo-items {
+  margin-top: 8px;
+  display: flex;
+  flex-direction: column;
+}
+
+.todo-list .todo-items > * {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
