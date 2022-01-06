56b002d6a1dc98c8c958ee3fa1f849e21a514138
InitRed <initred@gmail.com>
2022-01-07 03:36:22 +0900

    7 - code refactor & localStorage

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -12,63 +12,129 @@
               type="text" 
               id="todo-input"
               placeholder="add items"
+              v-model.trim="todoListText"
             />
           </div>
           <button
             class="bg-indigo-500 rounded-sm text-white text-sm px-4"
             type="button"
-            @click="add"
+            @click="todoListAdd"
           >
             추가
           </button>
         </div>
       </div>
-      <div 
-        v-if="items.length > 0"
-        class="flex flex-col space-y-2 p-4"
-      >
-        <div
-          class="flex items-center justify-between bg-white border border-gray-200 rounded-lg p-3 overflow-hidden"
-          v-for="(item, index) in items"
-          :key="index"
+      <div class="max-h-80 overflow-y-auto">
+        <div 
+          v-if="todoListItems.length > 0"
+          class="flex flex-col space-y-2 p-4"
         >
-          <p class="flex-1 overflow-hidden break-words">{{ item }}</p>
-          <button
-            class="bg-red-500 rounded-sm text-white py-2 px-4 text-sm ml-4" type="button" @click="remove"
+          <div
+            class="flex items-center justify-between bg-white border border-gray-200 rounded-lg p-3 overflow-hidden"
+            v-for="(item, index) in todoListItems"
+            :key="index"
           >
-            삭제
-          </button>
+            <div class="flex-1 flex items-center">
+              <div class="mr-2.5">
+                <input 
+                  type="checkbox" 
+                  class="rounded w-5 h-5 text-indigo-500"
+                  :checked="item.isCompleted"
+                  @input="(event) => todoListToggleCheck(event, index)"
+                > 
+              </div>
+              <p :class="['flex-1', { 'line-through' :item.isCompleted }]">{{ item.text }}</p>
+            </div>
+            
+            <button
+              class="bg-red-500 rounded-sm text-white py-2 px-4 text-sm ml-4" 
+              type="button" 
+              @click="todoListRemove(index)"
+            >
+              삭제
+            </button>
+          </div>
         </div>
+        <div
+        v-else
+        class="text-center py-12 bg-white text-sm text-gray-500"
+        >
+          할 일이 없습니다.
+          <br>
+          할 일을 추가해보세요.
+        </div>
+      </div>
+      <div class="flex items-center justify-between p-4">
+        <div class="bg-gray-500 text-white rounded text-sm">
+          <div class="py-2 px-4">
+          {{ todoListCompletedItemsLength }} / {{ todoListItems.length }}
+          </div>
+        </div>
+        <button 
+          type="button" 
+          class="bg-red-500 rounded-sm text-white py-2 px-4 text-sm ml-4"
+          @click="todoListCompletedItemsRemove"
+        >
+          완료된 할 일 모두 삭제
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { defineComponent, ref } from "vue";
+import { computed, defineComponent, onBeforeMount, onMounted, ref } from "vue";
 
 export default defineComponent({
   setup() {
-    const items = ref([]);
-
-    function add() {
-      console.log("add");
-
-      const todoText = document.getElementById("todo-input").value;
-
-      items.value.push(todoText);
+    const todoListText = ref('')
+    const todoListItems = ref([]);
+    const todoListCompletedItemsLength = computed(() => 
+      todoListItems.value.filter((item) => item.isCompleted).length
+    )
+    const todoListDefaultItemOptions = {
+      isCompleted: false,
+      isEdited: false,
     }
 
-    function remove(itemsIndex) {
-      console.log("remove");
-
-      items.value.splice(itemsIndex, 1);
+    function todoListAdd() {
+      if(todoListText.value) {
+        todoListItems.value.push({
+          text: todoListText.value,
+          ...todoListDefaultItemOptions,
+        });
+        localStorage.setItem('TodoList', JSON.stringify(todoListItems.value))
+        todoListText.value = ''
+      }
     }
+    
+    function todoListRemove(index) {
+      todoListItems.value.splice(index, 1);
+      localStorage.setItem('TodoList', JSON.stringify(todoListItems.value))
+    }
+
+    function todoListToggleCheck(event, index) {
+      todoListItems.value[index].isCompleted = event.target.checked
+    }
+
+    function todoListCompletedItemsRemove() {      
+      todoListItems.value = todoListItems.value.filter((item) => !item.isCompleted)
+    }
+
+    onBeforeMount(() => {
+      if(localStorage.getItem('TodoList')) {
+        todoListItems.value = JSON.parse(localStorage.getItem('TodoList'))
+      }
+    })
 
     return {
-      items,
-      add,
-      remove,
+      todoListText,
+      todoListItems,
+      todoListCompletedItemsLength,
+      todoListAdd,
+      todoListRemove,
+      todoListToggleCheck,
+      todoListCompletedItemsRemove,
     }
   }
 })
